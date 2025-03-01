72e356e95cdb2c43ebf0bd534fd57a123a48286a
Kkq96 <qurashi.kainat@gmail.com>
2025-03-01 15:04:06 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,11 @@
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open('pdf-cache').then((cache) => {
-            return cache.addAll([
-                'index.html',
-                'https://github.com/Kkq96/Ramadan-Dua-Guide/blob/main/Ramadan_Dua_Guide_KA.pdf'
-            ]);
-        })
-    );
+    console.log('Service Worker installing.');
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker activating.');
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+    console.log('Fetching:', event.request.url);
 });
-}
-
-
